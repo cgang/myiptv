5d7b2cf2a6acdb768e9fb90fd8871397d575ab57
Gang Chen <11877522+cgang@users.noreply.github.com>
2024-02-26 21:22:24 +0800

    Use OkHttp to download resource.

--- app/src/main/java/com/github/cgang/myiptv/Downloader.kt
+++ app/src/main/java/com/github/cgang/myiptv/Downloader.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.os.Handler
 import android.os.HandlerThread
 import android.util.Log
-import androidx.compose.runtime.key
 import com.github.cgang.myiptv.xmltv.ProgramParser
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import java.io.IOException
 import java.io.InputStream
 import java.io.InputStreamReader
@@ -15,14 +17,21 @@ import java.net.URL
 class Downloader(val context: Context) {
     private val TAG = Downloader::class.java.simpleName
     private val DefaultTimeout = 500 // seconds
-    val handler: Handler
     private var listener: PlaylistListener? = null
     private var channelIds = mutableSetOf<String>()
+    private val client: OkHttpClient
+    private val handler: Handler
 
     init {
         val thread = HandlerThread("download")
         thread.start()
         handler = Handler(thread.looper)
+
+        val cacheSize = 500L * 1024L * 1024L // 500 MiB
+        val cacheDir = context.externalCacheDir ?: context.cacheDir
+        client = OkHttpClient.Builder()
+            .cache(Cache(cacheDir, cacheSize))
+            .build()
     }
 
     fun register(l: PlaylistListener) {
@@ -48,7 +57,7 @@ class Downloader(val context: Context) {
             download(urlStr) {
                 var programs = ProgramParser().parse(it)
                 if (channelIds.isNotEmpty()) { // filter programs to save memory
-                    programs = programs.filterKeys { key -> channelIds.contains(key)}
+                    programs = programs.filterKeys { key -> channelIds.contains(key) }
                 }
                 listener?.onPrograms(programs)
             }
@@ -58,11 +67,16 @@ class Downloader(val context: Context) {
     private fun download(urlStr: String, handle: (input: InputStream) -> Unit) {
         try {
             Log.d(TAG, "Downloading ${urlStr}")
-            val url = URL(urlStr)
-            val conn = url.openConnection() as HttpURLConnection
-            conn.connectTimeout = DefaultTimeout
-            conn.inputStream.use(handle)
-            Log.d(TAG, "${urlStr} loaded successfully")
+            val request = Request.Builder().url(urlStr).build()
+            val response = client.newCall(request).execute()
+            if (response.isSuccessful) {
+                response.body!!.use {
+                    handle(it.byteStream())
+                }
+                Log.d(TAG, "${urlStr} loaded successfully")
+            } else {
+                Log.d(TAG, "Failed to download ${urlStr}: ${response.body!!.string()}")
+            }
         } catch (e: IOException) {
             Log.w(TAG, "Failed to download ${urlStr}: ${e}")
         } catch (e: Exception) {
