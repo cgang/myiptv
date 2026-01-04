6371b88ef26f34167ed3d725de998b8aa567bbfd
Gang Chen <11877522+cgang@users.noreply.github.com>
2026-01-04 18:36:35 +0800

    Bump version to 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 23
         targetSdk = 36
         versionCode = 10
-        versionName = "1.2.0"
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
